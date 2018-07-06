b8bd38b5ef5f1e8973cf94205ac1dcfd3cbc77b7
limingming <limingming@wident.cn>
2018-07-06 11:58:19 +0800

    run

--- stratum/run.sh
+++ stratum/run.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-cd /var/stratum/config/ && ./run.sh $*
+ulimit -n 10240
+ulimit -u 10240
+
+# cd /var/stratum
+while [ -e config/${1}.conf ]; do
+	gzip -f config/${1}.log
+        ./stratum config/$1
+	sleep 1
+done
+exec bash
 
