529b9d0f43953f6436e9f9b9434fb67fbc0dc3ae
limingming <limingming@wident.cn>
2018-07-06 18:49:12 +0800

    blake2s

--- stratum/client.cpp
+++ stratum/client.cpp
@@ -306,9 +306,10 @@ bool client_update_block(YAAMP_CLIENT *client, json_value *json_params)
 	int coinid = json_params->u.array.values[1]->u.integer;
 	debuglog("coinid %d\n",coinid);
 	if(!coinid) return false;
+	debuglog("client_update_block\n");
 	YAAMP_COIND *coind = (YAAMP_COIND *)object_find(&g_list_coind, coinid, true);
 	if(!coind) return false;
-	debuglog("client_update_block\n");
+	
 	const char* hash = json_params->u.array.values[2]->u.string.ptr;
 	debuglog("client.cpp 312");
 	if (g_debuglog_client) {
